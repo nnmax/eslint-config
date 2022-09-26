24731751714351bff9b7b0e110f2bb2fa114445a
nnmax <hi.max@foxemail.com>
2022-09-26 12:25:22 +0800

    chore: update

--- .releaserc.js
+++ .releaserc.js
@@ -1,11 +1,3 @@
 module.exports = {
-  branches: [
-    '+([0-9])?(.{+([0-9]),x}).x',
-    'main',
-    'semantic-release',
-    'next',
-    'next-major',
-    { name: 'beta', prerelease: true },
-    { name: 'alpha', prerelease: true },
-  ],
+  branches: ['main'],
 }
